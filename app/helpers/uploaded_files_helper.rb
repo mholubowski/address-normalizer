0853388244554c83f878cf3dccddfbc13638994d
Mike Holubowski <mike.holubowski@gmail.com>
2013-07-15 15:59:45 -0700

    Styled tables, refactores some bits

--- app/helpers/uploaded_files_helper.rb
+++ app/helpers/uploaded_files_helper.rb
@@ -1,7 +1,8 @@
 module UploadedFilesHelper
   def time_to_address_set row_count
     time_per_row = 0.015
-    seconds = row_count * time_per_row + 1
+    sql_time = 3
+    seconds = row_count * time_per_row * sql_time + 1 
     seconds = seconds.round
     if seconds < 60
       "#{seconds} seconds"
