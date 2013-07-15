1cedbb737ef0ae02e32910274273e54f89ee64dc
Mike Holubowski <mike.holubowski@gmail.com>
2013-07-15 10:48:56 -0700

    Starting on AddressSet#show
    -Creates _stats partial
    -.uniq_addresses methods
    - overloaded TokenizedAddreses equality methods

--- app/models/address_set.rb
+++ app/models/address_set.rb
@@ -2,4 +2,12 @@ class AddressSet < ActiveRecord::Base
   has_one :uploaded_file
   has_many :tokenized_addresses
 
+  def error_count
+    self.tokenized_addresses.where(address: 'ERROR').count
+  end
+
+  def uniq_addresses
+    self.tokenized_addresses.to_a.uniq
+  end
+
 end
--- app/models/tokenized_address.rb
+++ app/models/tokenized_address.rb
@@ -4,7 +4,7 @@ class TokenizedAddress < ActiveRecord::Base
   attr_accessor :init_string
   before_create :use_street_address_gem
 
-  private
+  # private
   def use_street_address_gem
     obj = StreetAddress::US.parse(@init_string, {informal: true})
     #TODO handle errors better
@@ -25,5 +25,37 @@ class TokenizedAddress < ActiveRecord::Base
     self.state            = obj.state
     self.postal_code      = obj.postal_code
     self.postal_code_ext  = obj.postal_code_ext
+  end
+
+  def == (other_object)
+    self.to_hash == other_object.to_hash
+  end
+
+  def to_hash
+    obj = {}
+
+    obj[:address]         = self.address
+    obj[:line1]           = self.line1
+    obj[:number]          = self.number
+    obj[:street]          = self.street
+    obj[:street_type]     = self.street_type
+    obj[:unit]            = self.unit
+    obj[:unit_prefix]     = self.unit_prefix
+    obj[:suffix]          = self.suffix
+    obj[:prefix]          = self.prefix
+    obj[:city]            = self.city
+    obj[:state]           = self.state
+    obj[:postal_code]     = self.postal_code
+    obj[:postal_code_ext] = self.postal_code_ext
+
+    return obj
+  end
+
+  def hash
+    self.to_hash.hash
+  end
+
+  def eql? (other_object)
+    self == other_object
   end
 end
